8ea6f3683cb4ffe391ff8e06e13f3835fb03e70a
Bruce <hulkmad>
2019-10-03 19:40:51 -0700

    I finished project 1.

--- src/DemoGit.java
+++ src/DemoGit.java
@@ -15,7 +15,7 @@ public class DemoGit
 	 */
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello. My name is William.");
+		System.out.println("Hello. My name is William");
 
 	}
 
